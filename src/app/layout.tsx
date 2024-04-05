eb72faae1f1caafd4b3426567e71d6fc71b6c324
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-04-05 08:12:21 +0200

    correct property spelling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
-        <meta charset='UTF-8' />
+        <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>Document</title>
       </head>
