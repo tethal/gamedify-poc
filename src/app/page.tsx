8d20cff8c27e8d9fcea24db19d45ec55792fdedd
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-07-03 11:34:33 +0200

    create small components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,17 @@ import Avatar from './Avatar';
 
 export default function Home() {
   return (
-    <>
+    <div className='h-[calc(100vh-60px)]  '>
       <nav className='flex justify-end mt-4 max-w-7xl w-full'>
         <Avatar />
       </nav>
-    <div className='flex flex-col justify-center items-center h-full w-full'>
-      <h1 className='text-5xl'>AZk game</h1>
-      <Link href='/quiz/' className='underline'>
-        List of quizzes (TODO hide unless authenticated)
-      </Link>
-      <EnterQuizCodeForm />
+      <div className='flex flex-col h-full  items-center lg:justify-center'>
+        <h1 className='text-5xl'>AZk game</h1>
+        <Link href='/quiz/' className='underline'>
+          List of quizzes (TODO hide unless authenticated)
+        </Link>
+        <EnterQuizCodeForm />
+      </div>
     </div>
-    </>
   );
 }
---
+++ src/app/play/[code]/PlayerOnTurn.tsx
@@ -0,0 +1,16 @@
+import { COLORS, type Player } from './defs';
+
+interface PlayerOnTurnProps{
+    playerOnTurn: Player,
+    className: string
+}
+
+export default function PlayerOnTurn({playerOnTurn, className}:PlayerOnTurnProps){
+    return(
+        <div
+              className={`${className} hidden lg:flex absolute  bg-[${COLORS[playerOnTurn]}] shadow-[0px_0px_10px_#fff,0px_0px_10px_5px_${COLORS[playerOnTurn]}] w-fit aspect-auto py-2 px-6 text-[#27272A] rounded-xl font-bold text-2xl`}
+            >
+              Player {playerOnTurn}
+            </div>
+    )
+}
