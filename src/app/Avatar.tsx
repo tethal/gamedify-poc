28cb7f17a72b143b1d1a108ff7b945a791532741
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-07-19 09:38:17 +0200

    dark/light mode

--- src/app/Avatar.tsx
+++ src/app/Avatar.tsx
@@ -15,7 +15,7 @@ export default async function Avatar() {
   } else {
     return (
       <Link
-        className='border px-6 py-1.5 rounded-full hover:text-cyan-500 hover:shadow-[0px_0px_10px_2px_#06B6D4] hover:border-cyan-950 '
+        className='border px-6 py-1.5 rounded-full z-30 hover:text-cyan-500 hover:shadow-[0px_0px_10px_2px_#06B6D4] hover:border-cyan-950 '
         href='/login/'
       >
         log in
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,29 @@
 import Link from 'next/link';
 import EnterQuizCodeForm from './EnterQuizCodeForm';
-import Avatar from './Avatar';
+import ThemeSwitcher from '@/components/ThemeSwitcher'
+import Avatar from './Avatar'
+
+
 import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
-    <div className='h-[calc(100vh-60px)]'>
-      <nav className='flex justify-end mt-4 max-w-7xl w-full'>
-        <Avatar />
-      </nav>
-      <div className='flex flex-col h-full  items-center sm:justify-center'>
-        <h1 className='text-5xl'>AZk game</h1>
-        {session !== null && (
+      <div className='flex flex-col items-center justify-center'>
+        <nav className='flex justify-end gap-4 max-w-7xl w-full'>
+          <Avatar />
+          <ThemeSwitcher />
+        </nav>
+        <div className="h-[90vh] flex flex-col justify-center items-center ">
+          <h1 className='text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-cyan-500 '>AZk game</h1>
+          {session !== null && (
           <Link href='/quiz/' className='underline'>
-            List of quizzes
-          </Link>
-        )}
+              List of quizzes
+            </Link>
+          )}
         <EnterQuizCodeForm />
+        </div>
       </div>
-    </div>
   );
 }
