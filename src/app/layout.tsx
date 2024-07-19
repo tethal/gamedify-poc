42bb7051b53a84929a364c76f34c9f4aca0b37ce
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-07-19 10:36:13 +0200

    add footer to bottom of the page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import ThemeSwitcher from '@/components/ThemeSwitcher'
+import GoHome from './play/[code]/GoHome';
+
 
 export const metadata: Metadata = {
   title: 'GamEdify PoC',
@@ -12,9 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className='bg-zinc-200 dark:bg-zinc-950 dark:text-white/70  w-[min(1200px,98%)] mx-auto h-dvh p-4 select-none relative'>
+      <body className='bg-zinc-200 dark:bg-zinc-950 dark:text-white/70  w-[min(1200px,98%)] mx-auto h-dvh grid grid-template-rows-[auto_1fr_auto] p-4 select-none relative'>
         {children}
       </body>
+      <footer className='max-w-7xl flex justify-end p-4 gap-4'>
+        <GoHome className={''} />
+        <ThemeSwitcher />
+      </footer>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,26 @@
 import Link from 'next/link';
 import EnterQuizCodeForm from './EnterQuizCodeForm';
-import ThemeSwitcher from '@/components/ThemeSwitcher'
 import Avatar from './Avatar'
-
-
 import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
-      <div className='flex flex-col items-center justify-center'>
-        <nav className='flex justify-end gap-4 max-w-7xl w-full'>
-          <Avatar />
-          <ThemeSwitcher />
-        </nav>
-        <div className="h-[90vh] flex flex-col justify-center items-center ">
-          <h1 className='text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-cyan-500 '>AZk game</h1>
-          {session !== null && (
-          <Link href='/quiz/' className='underline'>
-              List of quizzes
-            </Link>
-          )}
+    <div className='flex flex-col items-center justify-center'>
+      <nav className='flex justify-end gap-4 max-w-7xl w-full'>
+        <Avatar />
+      </nav>
+      <div className='h-[90vh] flex flex-col justify-center items-center '>
+        <h1 className='text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-cyan-500 '>
+          AZk game
+        </h1>
+        <Link href='/quiz/' className='underline'>
+          List of quizzes (TODO hide unless authenticated)
+        </Link>
         <EnterQuizCodeForm />
-        </div>
       </div>
+   
+    </div>
   );
 }
