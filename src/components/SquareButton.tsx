e1584b5ab1818d24f13cdc47da74c9ced0b25cda
Ondra Tethal <tethal@email.cz>
2024-08-28 21:24:22 +0200

    Oprava tlacitka

--- src/components/SquareButton.tsx
+++ src/components/SquareButton.tsx
@@ -2,19 +2,15 @@ import { ReactNode } from 'react';
 
 interface SquareButtonProps {
   children: ReactNode;
-  className?: string;
   onClick?: () => void;
-  type?: 'button' | 'submit' | 'reset';
 }
 
-const SquareButton = ({
-  children,
-  className,
-  onClick,
-  type,
-}: SquareButtonProps) => {
+const SquareButton = ({ children, onClick }: SquareButtonProps) => {
   return (
-    <button className='px-4 py-2 rounded-md bg-gray-800 text-white hover:text-cyan-500 hover:shadow-[0px_0px_10px_2px_#06B6D4]'>
+    <button
+      className='px-4 py-2 rounded-md bg-gray-800 text-white hover:text-cyan-500 hover:shadow-[0px_0px_10px_2px_#06B6D4]'
+      onClick={onClick}
+    >
       {children}
     </button>
   );
